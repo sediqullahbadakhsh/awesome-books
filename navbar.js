035f0db2b3f0f8d4d5bff5d266f143c07447b55a
Estebanmual <esteban.munozal@gmail.com>
2022-02-08 16:32:57 -0600

    Fix linters errors

--- navbar.js
+++ navbar.js
@@ -1,75 +1,87 @@
-//Navbar
+// Navbar
 
-const linkList = document.getElementById('list-link')
-const linkAdd = document.getElementById('add-link')
-const linkContact = document.getElementById('contact-link')
+const linkList = document.getElementById('list-link');
+const linkAdd = document.getElementById('add-link');
+const linkContact = document.getElementById('contact-link');
 
-const sectionList = document.getElementById('list')
-const sectionAdd = document.getElementById('add')
-const sectionContact = document.getElementById('contact')
+const sectionList = document.getElementById('list');
+const sectionAdd = document.getElementById('add');
+const sectionContact = document.getElementById('contact');
 
-linkList.addEventListener("click", () => {
-    if (sectionList.classList.contains('hidden')){
-        sectionList.classList.remove('hidden')
-        if (sectionAdd.classList.contains('hidden')){
-        } else {
-            sectionAdd.classList.add('hidden')
-        }
-        if (sectionContact.classList.contains('hidden')){
-        } else {
-            sectionContact.classList.add('hidden')
-        }
+linkList.addEventListener('click', () => {
+  if (sectionList.classList.contains('hidden')) {
+    sectionList.classList.remove('hidden');
+    if (sectionAdd.classList.contains('hidden')) {
+      return true;
     }
-    if (sectionAdd.classList.contains('hidden')){
-    } else {
-        sectionAdd.classList.add('hidden')
-    }
-    if (sectionContact.classList.contains('hidden')){
-    } else {
-        sectionContact.classList.add('hidden')
-    }
-})
+    sectionAdd.classList.add('hidden');
 
-linkAdd.addEventListener("click", () => {
-    if (sectionAdd.classList.contains('hidden')){
-        sectionAdd.classList.remove('hidden')
-        if (sectionList.classList.contains('hidden')){
-        } else {
-            sectionList.classList.add('hidden')
-        }
-        if (sectionContact.classList.contains('hidden')){
-        } else {
-            sectionContact.classList.add('hidden')
-        }
+    if (sectionContact.classList.contains('hidden')) {
+      return true;
     }
-    if (sectionList.classList.contains('hidden')){
-    } else {
-        sectionList.classList.add('hidden')
-    }
-    if (sectionContact.classList.contains('hidden')){
-    } else {
-        sectionContact.classList.add('hidden')
-    }
-})
+    sectionContact.classList.add('hidden');
+  }
+  if (sectionAdd.classList.contains('hidden')) {
+    return true;
+  }
+  sectionAdd.classList.add('hidden');
 
-linkContact.addEventListener("click", () => {
-    if (sectionContact.classList.contains('hidden')){
-        sectionContact.classList.remove('hidden')
-        if (sectionAdd.classList.contains('hidden')){
-        } else {
-            sectionAdd.classList.add('hidden')
-        }
-        if (sectionList.classList.contains('hidden')){
-        } else {
-            sectionList.classList.add('hidden')
-        }
+  if (sectionContact.classList.contains('hidden')) {
+    return true;
+  }
+  sectionContact.classList.add('hidden');
+
+  return true;
+});
+
+linkAdd.addEventListener('click', () => {
+  if (sectionAdd.classList.contains('hidden')) {
+    sectionAdd.classList.remove('hidden');
+    if (sectionList.classList.contains('hidden')) {
+      return true;
     }
-    if (sectionAdd.classList.contains('hidden')){
-    } else {
-        sectionAdd.classList.add('hidden')
+    sectionList.classList.add('hidden');
+
+    if (sectionContact.classList.contains('hidden')) {
+      return true;
     }
-    if (sectionList.classList.contains('hidden')){
-    } else {
-        sectionList.classList.add('hidden')
+    sectionContact.classList.add('hidden');
+  }
+  if (sectionList.classList.contains('hidden')) {
+    return true;
+  }
+  sectionList.classList.add('hidden');
+
+  if (sectionContact.classList.contains('hidden')) {
+    return true;
+  }
+  sectionContact.classList.add('hidden');
+
+  return true;
+});
+
+linkContact.addEventListener('click', () => {
+  if (sectionContact.classList.contains('hidden')) {
+    sectionContact.classList.remove('hidden');
+    if (sectionAdd.classList.contains('hidden')) {
+      return true;
     }
-})
+    sectionAdd.classList.add('hidden');
+
+    if (sectionList.classList.contains('hidden')) {
+      return true;
+    }
+    sectionList.classList.add('hidden');
+  }
+  if (sectionAdd.classList.contains('hidden')) {
+    return true;
+  }
+  sectionAdd.classList.add('hidden');
+
+  if (sectionList.classList.contains('hidden')) {
+    return true;
+  }
+  sectionList.classList.add('hidden');
+
+  return true;
+});
