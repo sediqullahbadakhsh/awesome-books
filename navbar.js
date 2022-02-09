230e20d8868a5ed599691b68034488deb380a854
Estebanmual <esteban.munozal@gmail.com>
2022-02-09 12:26:28 -0600

    Linters changes

--- navbar.js
+++ navbar.js
@@ -8,60 +8,59 @@ const sectionList = document.getElementById('list');
 const sectionAdd = document.getElementById('add');
 const sectionContact = document.getElementById('contact');
 
-const links = document.getElementsByClassName('links')
-const sections = document.getElementsByClassName('section')
-    
+const links = document.getElementsByClassName('links');
+const sections = document.getElementsByClassName('section');
+
 linkList.addEventListener('click', () => {
-  for (let i = 0; i < links.length; i += 1 ) {
+  for (let i = 0; i < links.length; i += 1) {
     if (links[i].classList.contains('active')) {
-      links[i].classList.remove('active')
-    }
-  } 
-  
-  linkList.classList.add('active') 
-  
-  for (let i = 0; i < sections.length; i += 1) {
-    if (!sections[i].classList.contains('hidden')){
-      sections[i].classList.add('hidden')
+      links[i].classList.remove('active');
     }
   }
 
-  sectionList.classList.remove('hidden')
+  linkList.classList.add('active');
+
+  for (let i = 0; i < sections.length; i += 1) {
+    if (!sections[i].classList.contains('hidden')) {
+      sections[i].classList.add('hidden');
+    }
+  }
+
+  sectionList.classList.remove('hidden');
 });
 
 linkAdd.addEventListener('click', () => {
-  for (let i = 0; i < links.length; i += 1 ) {
+  for (let i = 0; i < links.length; i += 1) {
     if (links[i].classList.contains('active')) {
-      links[i].classList.remove('active')
-    }
-  } 
-  
-  linkAdd.classList.add('active')
-  
-  for (let i = 0; i < sections.length; i += 1) {
-    if (!sections[i].classList.contains('hidden')){
-      sections[i].classList.add('hidden')
+      links[i].classList.remove('active');
     }
   }
 
-  sectionAdd.classList.remove('hidden')
+  linkAdd.classList.add('active');
 
+  for (let i = 0; i < sections.length; i += 1) {
+    if (!sections[i].classList.contains('hidden')) {
+      sections[i].classList.add('hidden');
+    }
+  }
+
+  sectionAdd.classList.remove('hidden');
 });
 
 linkContact.addEventListener('click', () => {
-  for (let i = 0; i < links.length; i += 1 ) {
+  for (let i = 0; i < links.length; i += 1) {
     if (links[i].classList.contains('active')) {
-      links[i].classList.remove('active')
-    }
-  } 
-  
-  linkContact.classList.add('active')
-  
-  for (let i = 0; i < sections.length; i += 1) {
-    if (!sections[i].classList.contains('hidden')){
-      sections[i].classList.add('hidden')
+      links[i].classList.remove('active');
     }
   }
 
-  sectionContact.classList.remove('hidden')
+  linkContact.classList.add('active');
+
+  for (let i = 0; i < sections.length; i += 1) {
+    if (!sections[i].classList.contains('hidden')) {
+      sections[i].classList.add('hidden');
+    }
+  }
+
+  sectionContact.classList.remove('hidden');
 });
